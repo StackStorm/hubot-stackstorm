031034aa78e91e11ce8ea3452d9f29e5359b0fe5
Tomaz Muraus <tomaz@tomaz.me>
2015-06-02 21:31:27 +0800

    Move over tests.`

---
+++ tests/test-formatcommand.js
@@ -0,0 +1,31 @@
+/*jshint quotmark:false*/
+/*global describe, it*/
+"use strict";
+
+var chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  formatcommand = require('../lib/format_command.js')
+
+describe('format_command', function() {
+  it('should create the right format with format and description',
+    function() {
+      var o = formatcommand(null, 'a', 'format', 'description');
+      expect(o).to.be.an('string');
+      expect(o).to.equal('format - description');
+    });
+
+  it('should create the right format with format',
+    function() {
+      var o = formatcommand(null, 'a', 'format', '');
+      expect(o).to.be.an('string');
+      expect(o).to.equal('format - ');
+    });
+
+  it('should throw an exception', function() {
+    assert.throws(function() {
+        formatcommand(null, 'a', '', 'description');
+      },
+      Error, 'format should be non-empty.');
+  });
+});
