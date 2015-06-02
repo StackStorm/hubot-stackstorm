08854a588e32dc8d9fb3ceb6445777453c1eb218
manasdk <manasdk@gmail.com>
2015-06-02 12:08:26 -0700

    Hubot test with a mock adapter
    * Added a mock adapter which is used to bootstrap the tests
    * Trivial tests to validate setup functionality

--- scripts/stackstorm.js
+++ scripts/stackstorm.js
@@ -35,10 +35,10 @@ limitations under the License.
 var url = require('url');
 
 var _ = require('lodash'),
-    util = require('util'),
-    env = process.env,
-    formatCommand = require('../lib/format_command.js'),
-    formatData = require('../lib/format_data.js');
+  util = require('util'),
+  env = process.env,
+  formatCommand = require('../lib/format_command.js'),
+  formatData = require('../lib/format_data.js');
 
 var st2client = require('st2client');
 
@@ -127,15 +127,14 @@ module.exports = function(robot) {
       request = request.header('X-Auth-Token', auth_token);
     }
 
-    request.get() (
+    request.get()(
       function(err, resp, body) {
         var parsed_body, success;
 
         parsed_body = JSON.parse(body);
         if (!_.isArray(parsed_body)) {
           success = false;
-        }
-        else {
+        } else {
           success = true;
         }
 
@@ -315,6 +314,7 @@ module.exports = function(robot) {
 
     // Initial command loading
     loadCommands();
+    return commands_load_interval;
   }
 
   // TODO: Use async.js or similar or organize this better
@@ -337,8 +337,7 @@ module.exports = function(robot) {
       config['auth']['host'] = parsed['hostname'];
       config['auth']['port'] = parsed['port'];
       config['auth']['protocol'] = parsed['protocol'].substring(0, (parsed['protocol'].length - 1));
-    }
-    else {
+    } else {
       parsed = url.parse(env.ST2_API);
 
       config['host'] = parsed['hostname'];
@@ -352,13 +351,12 @@ module.exports = function(robot) {
       robot.logger.debug('Successfully authenticated.');
       auth_token = result['token'];
 
-      start();
+      return start();
     }).catch(function(err) {
       robot.logger.error('Failed to authenticate: ' + err.message.toString());
       process.exit(2);
     });
-  }
-  else {
-    start();
+  } else {
+    return start();
   }
 };
---
+++ tests/test-st2bot-setup.js
@@ -0,0 +1,94 @@
+/*
+ Licensed to the StackStorm, Inc ('StackStorm') under one or more
+ contributor license agreements.  See the NOTICE file distributed with
+ this work for additional information regarding copyright ownership.
+ The ASF licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with
+ the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*jshint quotmark:false*/
+/*jshint -W030*/
+/*global describe, it, before, after*/
+"use strict";
+
+// needed to get all coffeescript modules to be loaded
+require('coffee-script/register');
+
+var expect = require("chai").expect,
+  path = require("path"),
+  Robot = require("hubot/src/robot"),
+  TextMessage = require("hubot/src/message").TextMessage;
+
+var disableLogger = true,
+  controlledLogger = function(msg) {};
+
+describe("stanley the StackStorm bot", function() {
+  var robot, user, adapter, st2bot, commands_load_interval;
+
+  before(function(done) {
+    robot = new Robot(null, "mock-adapter", true, "Hubot");
+
+    // Hack. Need a better solution than stubbing out methods.
+    if (disableLogger) {
+      robot.logger.error = controlledLogger;
+      robot.logger.warn = controlledLogger;
+      robot.logger.info = controlledLogger;
+      robot.logger.debug = controlledLogger;
+    }
+
+    robot.adapter.on("connected", function() {
+
+      // Load script under test
+      st2bot = require("../scripts/stackstorm");
+      commands_load_interval = st2bot(robot);
+
+      // Load help module
+      robot.loadFile(path.resolve('node_modules', 'hubot-help', 'src'), 'help.coffee');
+
+      user = robot.brain.userForId("1", {
+        name: "mocha",
+        room: "#mocha"
+      });
+
+      adapter = robot.adapter;
+      done();
+    });
+
+    robot.run();
+  });
+
+  after(function() {
+    clearInterval(commands_load_interval);
+    robot.server.close();
+    robot.shutdown();
+  });
+
+  it("responds when asked for help", function(done) {
+    adapter.on("send", function(envelope, strings) {
+      expect(strings[0]).to.be.an('string');
+      done();
+    });
+    adapter.receive(new TextMessage(user, "Hubot help"));
+  });
+
+  it("has listeners", function(done) {
+    expect(robot.listeners).to.have.length(2);
+    done();
+  });
+
+  it("has the right environment variables", function(done) {
+    expect(process.env.ST2_API).to.exist;
+    expect(process.env.ST2_CHANNEL).to.exist;
+    expect(process.env.ST2_COMMANDS_RELOAD_INTERVAL).to.exist;
+    done();
+  });
+});
