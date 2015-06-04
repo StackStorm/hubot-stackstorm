31b767ce2114f02526d788fc50dc31103002a657
Tomaz Muraus <tomaz@tomaz.me>
2015-06-04 20:41:43 +0800

    Fix race condition - we need to use fs.readFileSync.

--- tests/test-command-factory.js
+++ tests/test-command-factory.js
@@ -27,14 +27,9 @@ var fs = require('fs'),
   Robot = require('./dummy-robot.js'),
   formatCommand = require('../lib/format_command.js');
 
-var alias_fixtures;
+var ALIAS_FIXTURES = fs.readFileSync('tests/fixtures/aliases.json');
+ALIAS_FIXTURES = JSON.parse(ALIAS_FIXTURES);
 
-fs.readFile('tests/fixtures/aliases.json', function(err, data) {
-  if (err) {
-    throw err;
-  }
-  alias_fixtures = JSON.parse(data);
-});
 
 function getCleanCommandFactory() {
   var robot = new Robot(false);
@@ -46,7 +41,7 @@ describe('command factory', function() {
     var command_factory, alias;
 
     command_factory = getCleanCommandFactory();
-    alias = alias_fixtures[0];
+    alias = ALIAS_FIXTURES[0];
 
     command_factory.addCommand(
       formatCommand(null, alias.name, alias.formats[0], alias.description),
@@ -63,8 +58,8 @@ describe('command factory', function() {
     alias_count = 0;
     command_factory = getCleanCommandFactory();
 
-    for (idx_a = 0; idx_a < alias_fixtures.length; idx_a++) {
-      alias = alias_fixtures[idx_a];
+    for (idx_a = 0; idx_a < ALIAS_FIXTURES.length; idx_a++) {
+      alias = ALIAS_FIXTURES[idx_a];
       for (idx_f = 0; idx_f < alias.formats.length; idx_f++) {
         alias_format = alias.formats[idx_f];
         command_factory.addCommand(
@@ -83,7 +78,7 @@ describe('command factory', function() {
     var command_factory, alias, match;
 
     command_factory = getCleanCommandFactory();
-    alias = alias_fixtures[0];
+    alias = ALIAS_FIXTURES[0];
 
     command_factory.addCommand(
       formatCommand(null, alias.name, alias.formats[0], alias.description),
@@ -105,7 +100,7 @@ describe('command factory', function() {
     var command_factory, alias, alias_format, match, idx_f;
 
     command_factory = getCleanCommandFactory();
-    alias = alias_fixtures[1];
+    alias = ALIAS_FIXTURES[1];
 
     for (idx_f = 0; idx_f < alias.formats.length; idx_f++) {
       alias_format = alias.formats[idx_f];
