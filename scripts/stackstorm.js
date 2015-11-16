2fb3f448a89d5198ada02f88b8185add532ecda7
Edward Medvedev <edward.medvedev@gmail.com>
2015-11-16 16:53:25 +0600

    Dumb down: ack

--- scripts/stackstorm.js
+++ scripts/stackstorm.js
@@ -195,38 +195,12 @@ module.exports = function(robot) {
         // Until aliasexecution endpoint didn't get patched with proper status and output, work
         // around this curious design decision.
         if (err.status === 200) {
-          return { id: err.message };
+          msg.send(err.message);
+        } else {
+          msg.send(util.format('Error: %s', err.message));
         }
-
-        throw err;
-      })
-      .then(function (execution) {
-        if (action_alias.ack && action_alias.ack.enabled === false) {
-          return;
-        }
-
-        if (action_alias.ack && action_alias.ack.format) {
-          return action_alias.ack.format;
-        }
-
-        var history_url = utils.getExecutionHistoryUrl(execution.id);
-
-        var message = util.format(_.sample(START_MESSAGES), execution.id);
-
-        if (history_url) {
-          message += util.format(' (details available at %s)', history_url);
-        }
-
-        return message;
-      })
-      .then(function (message) {
-        if (message) {
-          msg.send(message);
-        }
-      })
-      .catch(function (err) {
-        msg.send(util.format('error : %s', err.message));
       });
+
   };
 
   robot.respond(/([\s\S]+?)$/i, function(msg) {
