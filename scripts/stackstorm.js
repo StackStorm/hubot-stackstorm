5817a22f367814d1c4e048e9a7715c033a0f6152
Charlotte St. John <charlotte.stjohn@sendgrid.com>
2016-01-12 23:24:45 -0800

    use send and messageRoom

--- scripts/stackstorm.js
+++ scripts/stackstorm.js
@@ -217,7 +217,8 @@ module.exports = function(robot) {
       'format': format_string,
       'command': command,
       'user': name,
-      'source_channel': msg.message.room || msg.message.user.reply_to,
+      'source_channel': msg.message.room,
+      'reply_to': msg.message.reply_to,
       'notification_route': env.ST2_ROUTE || 'hubot'
     };
     var sendAck = function (res) {
