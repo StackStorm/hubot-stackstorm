c9f426586ae6e18047ea27db90484b8610dc861f
anthony-shaw <anthony.p.shaw@gmail.com>
2016-03-23 14:30:25 +1100

    correct field name

--- scripts/stackstorm.js
+++ scripts/stackstorm.js
@@ -268,7 +268,7 @@ module.exports = function(robot) {
       name = msg.message.user.name[0];
     }
     if (robot.adapterName === "spark") {
-      room = String(msg.message.user.roomId);
+      room = String(msg.message.user.room);
       name = msg.message.user.name
     }
     var payload = {
